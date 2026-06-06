892a4ad0e57a21e0ecf65726700762a7e6d47a49
Fernanda019 <fernanda05@gmail.com>
2026-06-06 12:35:26 -0500

    Create todo list component

---
+++ src/components/ListItem.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+defineProps<{
+  isChecked?: boolean
+}>()
+</script>
+
+<template>
+  <label>
+    <input
+      type="checkbox"
+      :checked="isChecked"
+    >
+    <span>
+      <slot />
+    </span>
+  </label>
+</template>
+
+<style scoped>
+label {
+  cursor: pointer;
+}
+
+input:checked + span {
+  text-decoration: line-through;
+}
+</style>
